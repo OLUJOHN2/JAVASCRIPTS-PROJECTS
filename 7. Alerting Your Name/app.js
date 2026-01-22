637980e028e5aa473497fc9d8caa76b9f58ec5f0
OLUJOHN2 <olumoyepelumi1@gmail.com>
2026-01-22 19:42:41 +0100

    Todo List

--- 7. Alerting Your Name/app.js
+++
@@ -1,7 +0,0 @@
-const btnElement = document.querySelector('button');
-const spanElement = document.querySelector('span');
-
-btnElement.addEventListener('click', () => {
-    const yourName = prompt('please enter your name:')
-    spanElement.textContent = yourName;
-});
